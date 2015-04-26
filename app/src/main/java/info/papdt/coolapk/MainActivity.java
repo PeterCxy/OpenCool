fc0b68a76e73829f7001ec1b4918133de6c79e8c
Peter Cai <xqsx43cxy@126.com>
2015-04-26 15:18:33 +0800

    Initial commit

---
+++ app/src/main/java/info/papdt/coolapk/MainActivity.java
@@ -0,0 +1,14 @@
+package info.papdt.coolapk;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
